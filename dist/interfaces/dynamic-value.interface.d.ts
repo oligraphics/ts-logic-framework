76df31008f0cf2a36a999bfbdcfc77d2225442ff
Oliver Detig <11171071+oligraphics@users.noreply.github.com>
2024-12-14 11:14:35 +0100

    feat: recompile code

--- dist/interfaces/dynamic-value.interface.d.ts
+++ dist/interfaces/dynamic-value.interface.d.ts
@@ -1,4 +1,4 @@
 import { MathExpressionDto } from '../dto/expressions/math-expression.dto';
 import { ConditionalValueDto } from '../dto/conditionals/conditional-value.dto';
-export type DynamicValue = DynamicValue[] | MathExpressionDto | ConditionalValueDto | string | number | boolean | null;
+export type DynamicValue = DynamicValue[] | MathExpressionDto | ConditionalValueDto | object | string | number | boolean | null;
 //# sourceMappingURL=dynamic-value.interface.d.ts.map
--- dist/services/dynamic-context.service.js
+++ dist/services/dynamic-context.service.js
@@ -3,7 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DynamicContextService = void 0;
 exports.DynamicContextService = new (class DynamicContextService {
     createContext(props, variables) {
-        const result = {};
+        const result = {
+            ...props,
+        };
         for (const [key, value] of Object.entries(props)) {
             result[`{${key}}`] = value;
         }
