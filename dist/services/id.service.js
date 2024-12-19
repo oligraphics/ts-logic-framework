2e8eef8ca149bddcbbccbd517e35631fc8427d42
Oligraphics <11171071+oligraphics@users.noreply.github.com>
2024-12-19 23:20:59 +0100

    fix: id service should return a string, not an array of strings

--- dist/services/id.service.js
+++ dist/services/id.service.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.IdService = void 0;
-const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 exports.IdService = new (class IdService {
     createRandomId(length = 8) {
-        return Array.from({ length }).map(() => chars[Math.floor(Math.random() * chars.length)]);
+        return Array.from({ length })
+            .map(() => chars[Math.floor(Math.random() * chars.length)])
+            .join('');
     }
 })();
 //# sourceMappingURL=id.service.js.map
