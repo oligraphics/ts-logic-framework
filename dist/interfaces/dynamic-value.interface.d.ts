e1d7ea9f92623cc59d530a0a7d913a8bca0a0f6e
Oligraphics <11171071+oligraphics@users.noreply.github.com>
2025-01-07 07:43:52 +0100

    cleanup: deleted obsolete files

--- dist/interfaces/dynamic-value.interface.d.ts
+++
@@ -1,5 +0,0 @@
-import { MathExpressionDto } from '../dto/expressions/math-expression.dto';
-import { ConditionalValueDto } from '../dto/conditionals/conditional-value.dto';
-import { SelectionDto } from '../dto/selection/selection.dto';
-export type DynamicValue = DynamicValue[] | MathExpressionDto | ConditionalValueDto | SelectionDto | unknown;
-//# sourceMappingURL=dynamic-value.interface.d.ts.map
